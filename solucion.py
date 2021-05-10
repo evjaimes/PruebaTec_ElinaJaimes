d982457ae8def2a2dca972c2ba8cc0694a709a0e
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2021-05-10 18:28:12 -0500

    TERMINADO

--- solucion.py
+++ solucion.py
@@ -2,7 +2,32 @@
 # Universidad de los Andes
 
 def rangos(m, ranks):
-    return None
+    arreglo = [0 for i in range(m+1)]
+    for i in range(0, len(ranks), 2):
+        a = ranks[i]
+        b = ranks[i+1]
+        for j in range(a, b+1):
+            arreglo[j] = 1
+
+    # Solución
+    nuevosRangos = []
+    i = 0
+    while i < len(arreglo):
+        if arreglo[i] == 0:
+            pass
+        else:
+            uno = i
+            dos = None
+            for j in range(i, len(arreglo)):
+                if arreglo[j] == 1:
+                    dos = j
+                    arreglo[j] = 0
+                else:
+                    break
+            nuevosRangos.append((uno, dos))
+        i += 1
+
+    return nuevosRangos
 
 
 r = input(
